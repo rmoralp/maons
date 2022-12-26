452f216968ca8ad4a1f3eedaffa2741b03019eb0
rmoralp <rafamp89@gmail.com>
2022-12-26 17:55:01 +0100

    feat: improve lint usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ['./node_modules/@maons/lint'],
   settings: {
     next: {
-      rootDir: ["apps/*/"],
-    },
-  },
-};
+      rootDir: ['apps/*/']
+    }
+  }
+}
