da88524c057345569a0dffd6d225671d823282dc
rafa moral <rafamp89@gmail.com>
2023-01-23 12:42:21 +0100

    fix(react/components): add aria label and overable classNames

--- react/components/src/Tag/Tag.tsx
+++ react/components/src/Tag/Tag.tsx
@@ -5,18 +5,28 @@ import {defaultStyles, stylesByColor, stylesBySize} from './styles'
 import {TagColor, TagSize} from './types'
 
 interface TagProps {
+  className?: string
   color: TagColor
+  labelText?: string
   size?: TagSize
 }
 
 const Tag = ({
+  children,
+  className,
   color = TagColor.primary,
-  size = TagSize.sm,
-  children
+  labelText,
+  size = TagSize.sm
 }: PropsWithChildren<TagProps>) => {
   return (
     <span
-      className={cn(defaultStyles, stylesByColor[color], stylesBySize[size])}
+      className={cn(
+        defaultStyles,
+        stylesByColor[color],
+        stylesBySize[size],
+        className
+      )}
+      aria-label={labelText}
     >
       {children}
     </span>
