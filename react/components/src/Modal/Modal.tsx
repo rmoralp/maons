ac6d984495fe4707c7ae8b4bbf7a719601050b0e
rafa moral <rafamp89@gmail.com>
2023-02-07 21:50:07 +0100

    feat(react/components): animations for Modal

--- react/components/src/Modal/Modal.tsx
+++ react/components/src/Modal/Modal.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import {AnimatePresence, motion} from 'framer-motion'
 import {ComponentPropsWithoutRef, ElementRef, forwardRef} from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
 
 import {CloseIcon} from '../Icons/Close'
 import {cn} from '../utils/cn'
+import {contentAnimation} from './animations'
 
-const Modal = Dialog.Root
+const Modal = ({children}: Dialog.DialogProps) => {
+  return (
+    <AnimatePresence>
+      <Dialog.Root>{children}</Dialog.Root>
+    </AnimatePresence>
+  )
+}
 
 const ModalTrigger = Dialog.Trigger
 
@@ -33,7 +41,16 @@ const ModalOverlay = forwardRef<
     className={cn('fixed inset-0 z-20 bg-black/50', className)}
     {...props}
     ref={ref}
-  />
+    asChild
+    forceMount
+  >
+    <motion.div
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+      transition={{duration: 0.05}}
+    />
+  </Dialog.Overlay>
 ))
 
 ModalOverlay.displayName = Dialog.Overlay.displayName
@@ -41,34 +58,46 @@ ModalOverlay.displayName = Dialog.Overlay.displayName
 const ModalContent = forwardRef<
   ElementRef<typeof Dialog.Content>,
   ComponentPropsWithoutRef<typeof Dialog.Content>
->(({className, children, ...props}, ref) => (
-  <ModalPortal>
-    <ModalOverlay forceMount />
-    <Dialog.Content
-      ref={ref}
-      className={cn(
-        'fixed z-50',
-        'w-[95vw] max-w-md rounded-lg p-4 md:w-full',
-        'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
-        'bg-white dark:bg-gray-800',
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <Dialog.Close
+>(({className, children, ...props}, ref) => {
+  return (
+    <ModalPortal>
+      <ModalOverlay />
+      <Dialog.Content
+        ref={ref}
         className={cn(
-          'absolute top-2 right-2 inline-flex items-center justify-center rounded-full p-1',
-          'transition-all hover:bg-gray-100',
-          'focus:ring-primary-500 focus:outline-none focus:ring focus:ring-opacity-75'
+          'fixed z-50',
+          'w-[95vw] max-w-md rounded-lg p-4 md:w-full',
+          'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
+          'bg-white dark:bg-gray-800',
+          className
         )}
+        {...props}
+        asChild
+        forceMount
       >
-        <CloseIcon className="h-6 w-6" />
-        <span className="sr-only">Close</span>
-      </Dialog.Close>
-    </Dialog.Content>
-  </ModalPortal>
-))
+        <motion.div
+          variants={contentAnimation}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          style={{zIndex: 50}}
+        >
+          {children}
+          <Dialog.Close
+            className={cn(
+              'absolute top-2 right-2 inline-flex items-center justify-center rounded-full p-1',
+              'transition-all hover:bg-gray-100',
+              'focus:ring-primary-500 focus:outline-none focus:ring focus:ring-opacity-75'
+            )}
+          >
+            <CloseIcon className="h-6 w-6" />
+            <span className="sr-only">Close</span>
+          </Dialog.Close>
+        </motion.div>
+      </Dialog.Content>
+    </ModalPortal>
+  )
+})
 
 ModalContent.displayName = Dialog.Content.displayName
 
