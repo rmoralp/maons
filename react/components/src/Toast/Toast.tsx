4d72c5109df6ecd72fa070dea578afe0d8b16bb2
rafa moral <rafamp89@gmail.com>
2023-02-08 14:34:53 +0100

    feat(react/components): add motion to Toast

--- react/components/src/Toast/Toast.tsx
+++ react/components/src/Toast/Toast.tsx
@@ -1,3 +1,5 @@
+import {motion} from 'framer-motion'
+
 import * as T from '@radix-ui/react-toast'
 
 import {CloseIcon} from '../Icons/Close'
@@ -24,19 +26,37 @@ export const Toast = ({
   ...props
 }: ToastProps) => {
   return (
-    <T.Root className={rootStyles} duration={duration} {...props}>
-      {status && <ToastIcon status={status} />}
-      <div>
-        <T.Title className={cn(titleStyles)}>{title}</T.Title>
-        <T.Description className={cn(descriptionStyles)}>
-          {description}
-        </T.Description>
-      </div>
-      <T.Close asChild>
-        <div className={closeStyles}>
-          <CloseIcon className="h-6 w-6 cursor-pointer" />
+    <T.Root duration={duration} {...props} asChild forceMount open>
+      <motion.li
+        className={rootStyles}
+        initial={{opacity: 0, scale: 0.95, y: 24}}
+        animate={{
+          y: 0,
+          opacity: 1,
+          scale: 1,
+          transition: {duration: 0.1, ease: 'easeOut'}
+        }}
+        exit={{
+          opacity: 0,
+          scale: 0.95,
+          y: 10,
+          transition: {duration: 0.1, ease: 'easeOut'}
+        }}
+        transition={{duration: 0.2, ease: 'easeOut'}}
+      >
+        {status && <ToastIcon status={status} />}
+        <div>
+          <T.Title className={cn(titleStyles)}>{title}</T.Title>
+          <T.Description className={cn(descriptionStyles)}>
+            {description}
+          </T.Description>
         </div>
-      </T.Close>
+        <T.Close asChild>
+          <div className={closeStyles}>
+            <CloseIcon className="h-6 w-6 cursor-pointer" />
+          </div>
+        </T.Close>
+      </motion.li>
     </T.Root>
   )
 }
